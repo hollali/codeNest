69f3a325c80d3417eb899668852cd53425bd8f5b
hollali <dheztinykartel@gmail.com>
2024-11-03 09:37:05 +0000

    "Added Hero component import and element to App component"

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
 import Navbar from './sections/navbar'
+import Hero from './sections/hero'
 
 const App = () => {
   return (
     <main className='max-w-7xl mx-auto'>
       <h1 className='text-2xl text-white underline'>
         <Navbar/>
+        <Hero/>
       </h1>
     </main>
   )
