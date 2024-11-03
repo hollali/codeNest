a43218493cb4e549197aa9b07fcbb29fbc2534b1
hollali <dheztinykartel@gmail.com>
2024-11-03 09:38:56 +0000

    Remove unnecessary h1 element and its contents from App component.

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,8 @@ import Hero from './sections/hero'
 const App = () => {
   return (
     <main className='max-w-7xl mx-auto'>
-      <h1 className='text-2xl text-white underline'>
         <Navbar/>
         <Hero/>
-      </h1>
     </main>
   )
 }
