9c623b0dae156cf38e765ea4a4021158c2f4ebed
hollali <dheztinykartel@gmail.com>
2024-11-03 10:00:15 +0000

    "Updated Hero component with new JSX structure and styles"

--- src/sections/hero.jsx
+++ src/sections/hero.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
-
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='min-h-screen border-2 border-blue-500 w-full flex-col relative'>
+        <div className='w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3'>
+            <p className='sm:text-3xl text-xl font-medium text-white text-center font-generalsans'>Hi, we are CodeNest <span className='waving-hand'>👋</span></p>
+            <p className='hero_tag text-gray_gradient'>Building Products & Brands</p>
+        </div>
+    </section>
   )
 }
 
