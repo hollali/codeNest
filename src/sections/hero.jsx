2dec097c338409de27067ce585124bd9cdc16649
hollali <dheztinykartel@gmail.com>
2024-11-04 18:45:11 +0000

    "Refactor Hero component: added new media queries, calculated sizes, and updated camera position and scale"

--- src/sections/hero.jsx
+++ src/sections/hero.jsx
@@ -5,9 +5,15 @@ import { Suspense } from "react";
 import CanvasLoader from "../components/canvasLoader";
 import { Leva } from "leva";
 import { useMediaQuery } from "react-responsive";
+import { calculateSizes } from '../constants/index.js';
 
 const Hero = () => {
-	const isMobile = useMediaQuery({ maxWidth: 768 })
+	const isSmall = useMediaQuery({maxWidth: 440});
+	const isMobile = useMediaQuery({ maxWidth: 768 });
+	const isTablet = useMediaQuery({minWidth: 768, maxWidth: 1024});
+
+	const sizes = calculateSizes(isSmall, isMobile, isTablet);
+
 	return (
 		<section className="min-h-screen w-full  flex-col relative">
 			<div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
@@ -19,16 +25,17 @@ const Hero = () => {
 				</p>
 			</div>
 			<div className="w-full h-full absolute inset-0">
-			<Leva/>
+			    <Leva/>
 				<Canvas className="w-full h-full">
-					<Suspense fallback={<CanvasLoader />}>
-						<PerspectiveCamera makeDefault position={[0, 0, 30]} />
+					<Suspense fallback={<CanvasLoader/>}>
+						<PerspectiveCamera makeDefault position={[0, 0, 20]} />
 						<HackerRoom
-						position={[2, -8, 2]}
+						position={[sizes.deskPosition]}
 						rotation={[0, -Math.PI, 0]}
-						scale={isMobile ? 0.07 : 0.1}/>
+						scale={sizes.deskScale}/>
 						<ambientLight intensity={1}/>
-						<directionalLight position={[10,10,10]} intensity={0.5}/>
+						<directionalLight position={[10,10,10]} intensity={0.5}
+						/>
 					</Suspense>
 				</Canvas>
 			</div>
