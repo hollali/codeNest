ab4c0321f7b04220986048ab5073c9a1f7f37f9d
hollali <dheztinykartel@gmail.com>
2024-11-04 15:12:23 +0000

    "Removed Leva controls and updated HackerRoom props in hero.jsx; added newline at end of index.html"

--- src/sections/hero.jsx
+++ src/sections/hero.jsx
@@ -3,18 +3,11 @@ import { Canvas } from "@react-three/fiber";
 import HackerRoom from "../components/hackerRoom";
 import { Suspense } from "react";
 import CanvasLoader from "../components/canvasLoader";
-import { Leva, useControls } from "leva";
+import { Leva } from "leva";
+import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
-	const x = useControls('HackerRoom',{
-		positionX:{value:2.5,min:-10,max:10},
-		positionY:{value:2.5,min:-10,max:10},
-		positionZ:{value:2.5,min:-10,max:10},
-		rotationX:{value:0,min:-10,max:10},
-		rotationY:{value:0,min:-10,max:10},
-		rotationZ:{value:0,min:-10,max:10},
-		scale:{value:0,min:0.1,max:10},
-	})
+	const isMobile = useMediaQuery({ maxWidth: 768 })
 	return (
 		<section className="min-h-screen w-full  flex-col relative">
 			<div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
@@ -30,12 +23,10 @@ const Hero = () => {
 				<Canvas className="w-full h-full">
 					<Suspense fallback={<CanvasLoader />}>
 						<PerspectiveCamera makeDefault position={[0, 0, 30]} />
-						<HackerRoom  
-						//position={[0, 0, 0]} 
-						//rotation={[0, 280, 0]} 
-						position={[x.positionX, x.positionY,x.positionZ]}
-						rotation={[x.rotationX, x.rotationY,x.rotationZ]}
-						scale={[x.scale,x.scale,x.scale]}/>
+						<HackerRoom
+						position={[2, -8, 2]}
+						rotation={[0, -Math.PI, 0]}
+						scale={isMobile ? 0.07 : 0.1}/>
 						<ambientLight intensity={1}/>
 						<directionalLight position={[10,10,10]} intensity={0.5}/>
 					</Suspense>
