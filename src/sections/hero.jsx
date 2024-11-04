0d2df34c01b22fcac674eeb026ae24349a813215
hollali <dheztinykartel@gmail.com>
2024-11-04 03:31:08 +0000

    "Removed border styles from Hero section and capitalized 'We' in hero text"

--- src/sections/hero.jsx
+++ src/sections/hero.jsx
@@ -6,10 +6,10 @@ import CanvasLoader from "../components/canvasLoader";
 
 const Hero = () => {
 	return (
-		<section className="min-h-screen border-2 border-orange-400 w-full flex-col relative">
+		<section className="min-h-screen w-full  flex-col relative">
 			<div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
 				<p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">
-					Hi, we are CodeNest <span className="waving-hand">👋</span>
+					Hi, We are CodeNest <span className="waving-hand">👋</span>
 				</p>
 				<p className="hero_tag text-gray_gradient">
 					Building Products & Brands
