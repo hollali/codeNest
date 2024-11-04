d619d7583a78e8f85f5de133b07ba3d481690c14
hollali <dheztinykartel@gmail.com>
2024-11-04 02:07:09 +0000

    added hero section

--- src/sections/hero.jsx
+++ src/sections/hero.jsx
@@ -1,12 +1,32 @@
-const Hero = () => {
-  return (
-    <section className='min-h-screen border-2 border-blue-500 w-full flex-col relative'>
-        <div className='w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3'>
-            <p className='sm:text-3xl text-xl font-medium text-white text-center font-generalsans'>Hi, we are CodeNest <span className='waving-hand'>👋</span></p>
-            <p className='hero_tag text-gray_gradient'>Building Products & Brands</p>
-        </div>
-    </section>
-  )
-}
+import { PerspectiveCamera } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import HackerRoom from "../components/hackerRoom";
+import { Suspense } from "react";
+import CanvasLoader from "../components/canvasLoader";
 
-export default Hero
+const Hero = () => {
+	return (
+		<section className="min-h-screen border-2 border-orange-400 w-full flex-col relative">
+			<div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+				<p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">
+					Hi, we are CodeNest <span className="waving-hand">👋</span>
+				</p>
+				<p className="hero_tag text-gray_gradient">
+					Building Products & Brands
+				</p>
+			</div>
+			<div className="w-full h-full absolute inset-0">
+				<Canvas className="w-full h-full">
+					<Suspense fallback={<CanvasLoader />}>
+						<PerspectiveCamera makeDefault position={[0, 0, 30]} />
+						<HackerRoom scale={0.07} position={[0, 0, 0]} rotation={[0, 60, 0]} />
+						<ambientLight intensity={1}/>
+						<directionalLight position={[10,10,10]} intensity={0.5}/>
+					</Suspense>
+				</Canvas>
+			</div>
+		</section>
+	);
+};
+
+export default Hero;
