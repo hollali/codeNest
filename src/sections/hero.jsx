358790b5fa98317d592407ef12ee6ee2323ec6cc
hollali <dheztinykartel@gmail.com>
2024-11-04 18:54:20 +0000

    "Removed Leva import and comment, updated font size and added scale prop to HackerRoom component, and modified rotation value."

--- src/sections/hero.jsx
+++ src/sections/hero.jsx
@@ -3,7 +3,7 @@ import { Canvas } from "@react-three/fiber";
 import HackerRoom from "../components/hackerRoom";
 import { Suspense } from "react";
 import CanvasLoader from "../components/canvasLoader";
-import { Leva } from "leva";
+//import { Leva } from "leva";
 import { useMediaQuery } from "react-responsive";
 import { calculateSizes } from '../constants/index.js';
 
@@ -17,7 +17,7 @@ const Hero = () => {
 	return (
 		<section className="min-h-screen w-full  flex-col relative">
 			<div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
-				<p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">
+				<p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
 					Hi, We are CodeNest <span className="waving-hand">👋</span>
 				</p>
 				<p className="hero_tag text-gray_gradient">
@@ -25,14 +25,15 @@ const Hero = () => {
 				</p>
 			</div>
 			<div className="w-full h-full absolute inset-0">
-			<Leva/>
+			{/*<Leva/>*/}
 				<Canvas className="w-full h-full">
 					<Suspense fallback={<CanvasLoader/>}>
 						<PerspectiveCamera makeDefault position={[0, 0, 20]} />
 						<HackerRoom
+						scale={sizes.deskScale}
 						position={[sizes.deskPosition]}
-						rotation={[0, -Math.PI, 0]}
-						scale={sizes.deskScale}/>
+						rotation={[0.1, -Math.PI, 0]}
+						/>
 						<ambientLight intensity={1}/>
 						<directionalLight position={[10,10,10]} intensity={0.5}
 						/>
