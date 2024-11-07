3e196b8a69e5a01ba877aebe022219eaf4cb8539
hollali <dheztinykartel@gmail.com>
2024-11-07 21:15:42 +0000

    changed website logo

--- src/sections/project.jsx
+++ src/sections/project.jsx
@@ -3,7 +3,6 @@ import { useGSAP } from "@gsap/react";
 import { Suspense, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { Center, OrbitControls } from "@react-three/drei";
-
 import { myProjects } from "../constants/index.js";
 import CanvasLoader from "../components/loading.jsx";
 import DemoComputer from "../components/demoComputer.jsx";
@@ -12,7 +11,6 @@ const projectCount = myProjects.length;
 
 const Projects = () => {
 	const [selectedProjectIndex, setSelectedProjectIndex] = useState(0);
-
 	const handleNavigation = (direction) => {
 		setSelectedProjectIndex((prevIndex) => {
 			if (direction === "previous") {
@@ -36,7 +34,6 @@ const Projects = () => {
 	return (
 		<section className="c-space my-20">
 			<p className="head-text">My Selected Work</p>
-
 			<div className="grid lg:grid-cols-2 grid-cols-1 mt-12 gap-5 w-full">
 				<div className="flex flex-col gap-5 relative sm:p-10 py-10 px-5 shadow-2xl shadow-black-200">
 					<div className="absolute top-0 right-0">
@@ -46,7 +43,6 @@ const Projects = () => {
 							className="w-full h-96 object-cover rounded-xl"
 						/>
 					</div>
-
 					<div
 						className="p-3 backdrop-filter backdrop-blur-3xl w-fit rounded-lg"
 						style={currentProject.logoStyle}>
@@ -56,16 +52,13 @@ const Projects = () => {
 							alt="logo"
 						/>
 					</div>
-
 					<div className="flex flex-col gap-5 text-white-600 my-5">
 						<p className="text-white text-2xl font-semibold animatedText">
 							{currentProject.title}
 						</p>
-
 						<p className="animatedText">{currentProject.desc}</p>
 						<p className="animatedText">{currentProject.subdesc}</p>
 					</div>
-
 					<div className="flex items-center justify-between flex-wrap gap-5">
 						<div className="flex items-center gap-3">
 							{currentProject.tags.map((tag, index) => (
